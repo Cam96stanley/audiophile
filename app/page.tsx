301e1e7b0562f229d5daf664f8561e217280752c
Cameron Stanley <cam96stanley@gmail.com>
2026-01-19 03:06:12 -0500

    created mobile hero

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,17 @@ import styles from "./page.module.scss";
 export default function Home() {
   return (
     <div className={styles.page}>
-      <h1>Home Page</h1>
+      <main className={styles.hero}>
+        <div className={styles.content}>
+          <p className={styles.subtitle}>New Product</p>
+          <h1 className={styles.title}>XX99 Mark II Headphones</h1>
+          <p className={styles.body}>
+            Experience natural, lifelike audio and exceptional build quality
+            made for the passionate music enthusiast.
+          </p>
+          <button className={styles.products__button}>See Product</button>
+        </div>
+      </main>
     </div>
   );
 }
